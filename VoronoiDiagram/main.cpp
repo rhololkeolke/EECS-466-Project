b3d28c321c4c1f1657d6aaa955fc3f77c38d8847
Devin Schwab <digidevin@gmail.com>
2013-11-06 15:13:16 -0500

    Basic GLUT/OpenGL setup complete.

--- VoronoiDiagram/main.cpp
+++ VoronoiDiagram/main.cpp
@@ -1,8 +1,65 @@
+#include "GL/glut.h"
 #include <iostream>
+
+int WindowWidth = 320;
+int WindowHeight = 320;
+
+void DisplayFunc(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glutSwapBuffers();
+}
+
+void MouseFunc(int button,int state,int x,int y)
+{
+
+}
+
+void MotionFunc(int x, int y)
+{
+
+}
+
+void KeyboardFunc(unsigned char key, int x, int y)
+{
+	switch(key)
+	{
+	case 'Q':
+	case 'q':
+		exit(0);
+		break;
+	}
+}
+
+void ReshapeFunc(int x,int y)
+{
+    glViewport(0,0,x,y);
+    WindowWidth = x;
+    WindowHeight = y;
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello world" << std::endl;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(WindowWidth, WindowHeight);
+	glutCreateWindow("Voronoi Diagram");
+
+	glutDisplayFunc(DisplayFunc);
+	glutReshapeFunc(ReshapeFunc);
+	glutMouseFunc(MouseFunc);
+    glutMotionFunc(MotionFunc);
+    glutKeyboardFunc(KeyboardFunc);
+
+	glutMainLoop();
 
 	return 0;
 }
