24bec68e552215c066cb017292a1297fb71e1e4c
Devin Schwab <digidevin@gmail.com>
2013-11-07 19:25:19 -0500

    Added a typedef for the shared_ptr type of Point

--- VoronoiDiagram/point.h
+++ VoronoiDiagram/point.h
@@ -1,6 +1,8 @@
 #ifndef VORONOIDIAGRAM_POINT_H_
 #define VORONOIDIAGRAM_POINT_H_
 
+#include <memory>
+
 namespace voronoi_diagram
 {
 	typedef struct Point_
@@ -11,6 +13,8 @@ namespace voronoi_diagram
 			x(x), y(y)
 		{}
 	} Point;
+
+	typedef std::shared_ptr<Point> PointPtr;
 }
 
 #endif
