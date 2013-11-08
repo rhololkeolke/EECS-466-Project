8019513ef960bd7163622df0bbe80a4ac1cfe5d2
Devin Schwab <digidevin@gmail.com>
2013-11-07 21:10:57 -0500

    Added a placeholder for the Edge Class

---
+++ VoronoiDiagram/edge.h
@@ -0,0 +1,16 @@
+#ifndef VORONOIDIAGRAM_EDGE_H_
+#define VORONOIDIAGRAM_EDGE_H_
+
+#include <memory>
+
+namespace voronoi_diagram
+{
+	typedef struct Edge_
+	{
+		// TODO: implement me
+	} Edge;
+
+	typedef std::shared_ptr<Edge> EdgePtr;
+}
+
+#endif
