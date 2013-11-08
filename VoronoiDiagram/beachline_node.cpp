a38611f7f50c5457f7d9ce5b28a4a4de0acb73ed
Devin Schwab <digidevin@gmail.com>
2013-11-08 14:00:36 -0500

    Fixed the logic in the getLeftArc and getRightArc methods. They did not work properly when the supplied beachlinenode is an arc.

--- VoronoiDiagram/beachline_node.cpp
+++ VoronoiDiagram/beachline_node.cpp
@@ -8,16 +8,28 @@ namespace voronoi_diagram
 		std::shared_ptr<BeachlineNode> curr_node;
 		if(node->type_ == BeachlineNodeType::ARC) // if this is an arc
 		{
-			// first go up to the left edge
+			// initialize last_node to the input 
+			std::shared_ptr<BeachlineNode> last_node = node;
+			// initialize curr_node to the input's parent
 			curr_node = node->getParent();
-			if(!curr_node) // if this is the root 
-				return nullptr; // there is nothing to the left of this
-			curr_node = curr_node->getLeftChild(); // otherwise start going down the edges to the left of this node's left edge
+
+			while(curr_node && curr_node->getLeftChild().get() == last_node.get())
+			{
+				last_node = curr_node;
+				curr_node = curr_node->getParent();
+			}
+
+			// if we traversed all the way to the root without finding a new left branch
+			// then there is no arc to the left of the one specified
+			if(!curr_node)
+				return nullptr;
+
+			// go down the new left branch
+			curr_node = curr_node->getLeftChild();
 		}
 		else
 			curr_node = node->getLeftChild(); // no need to go up if we're starting on an edge
-		if(!curr_node)
-			return nullptr;
+
 		while(curr_node->type_ != BeachlineNodeType::ARC)
 		{
 			curr_node = curr_node->getRightChild();
@@ -31,9 +43,23 @@ namespace voronoi_diagram
 		std::shared_ptr<BeachlineNode> curr_node;
 		if(node->type_ == BeachlineNodeType::ARC)
 		{
+			// initialize last_node to the input
+			std::shared_ptr<BeachlineNode> last_node = node;
+			// initialize curr_node to the input's parent
 			curr_node = node->getParent();
+
+			while(curr_node && curr_node->getRightChild().get() == last_node.get())
+			{
+				last_node = curr_node;
+				curr_node = curr_node->getParent();
+			}
+
+			// if we traversed all the way to the root without finding a new right branch
+			// then there is no arc to the right of the one specified
 			if(!curr_node)
 				return nullptr;
+
+			// go down the new right branch
 			curr_node = curr_node->getRightChild();
 		}
 		else
