fc4313b547628065203a62a0dcbb9adda573bf73
Devin Schwab <digidevin@gmail.com>
2013-12-11 07:19:42 -0800

    Bounding box calculations are working. Also have an untested mesh triangle intersection routine.

--- src/mesh_sampler.cpp
+++ src/mesh_sampler.cpp
@@ -1,18 +1,55 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cmath>
+#include <cfloat>
 
 #include <tiny_obj_loader.h>
 
 #include <vector>
 #include <string>
+#include <memory>
 
 using std::vector;
 using std::string;
 using tinyobj::shape_t;
 using tinyobj::mesh_t;
 
-void calculateNormals(mesh_t& mesh);
+typedef struct Vector_
+{
+	float i, j, k;
+} Vector;
+
+typedef struct Point_
+{
+	float x, y, z;
+} Point;
+
+typedef Point Vertex;
+
+typedef struct BoundingBox_
+{
+	float x_min, x_max;
+	float y_min, y_max;
+	float z_min, z_max;
+} BoundingBox;
+
+inline Vector cross(const Vector& v1, const Vector& v2)
+{
+	Vector result;
+	result.i = v1.j*v2.k - v1.k*v2.j;
+	result.j = v1.k*v2.i - v1.i*v2.k;
+	result.k = v1.i*v2.j - v1.j*v2.i;
+	return result;
+}
+
+inline float dot(const Vector& v1, const Vector& v2)
+{
+	return v1.i*v2.i + v1.j*v2.j + v1.k*v2.k;
+}
+
+std::shared_ptr<Point> intersectMesh(const Point& start, const Vector& dir, const Vertex& v0, const Vertex& v1, const Vertex& v2);
+
+BoundingBox findBoundingBox(const vector<float>& vertices);
 
 int main(int argc, char** argv)
 {
@@ -52,36 +89,81 @@ int main(int argc, char** argv)
 		return 2;
 	}
 
-	printf("Number of shapes: %ul\n", shapes.size());
+	printf("Number of shapes: %d\n", shapes.size());
 
-	for(vector<shape_t>::const_iterator shape = shapes.begin();
-		shape != shapes.end();
-		shape++)
+	if(shapes.size() == 0)
 	{
-		printf("positions: %ul\n", shape->mesh.positions.size());
-		printf("normals: %ul\n", shape->mesh.normals.size());
-		printf("texcoords: %ul\n", shape->mesh.texcoords.size());
-		printf("indices: %ul\n", shape->mesh.indices.size());
-
-		printf("positions:\n");
-		for(vector<float>::const_iterator position = shape->mesh.positions.begin();
-			position != shape->mesh.positions.end();
-			position++)
-		{
-			printf("\t %3.3f\n", *position);
-		}
-
-		printf("indices:\n");
-		for(vector<unsigned int>::const_iterator index = shape->mesh.indices.begin();
-			index != shape->mesh.indices.end();
-			index++)
-		{
-			printf("\t %d\n", *index);
-		}
+		fprintf(stderr, "Mesh file did not contain any shapes...");
+		return 3;
 	}
+
+	BoundingBox bounding_box = findBoundingBox(shapes[0].mesh.positions);
+
+	printf("Bounding box:\n");
+	printf("\t [%3.3f, %3.3f]\n", bounding_box.x_min, bounding_box.x_max);
+	printf("\t [%3.3f, %3.3f]\n", bounding_box.y_min, bounding_box.y_max);
+	printf("\t [%3.3f, %3.3f]\n", bounding_box.z_min, bounding_box.z_max);
+	
 }
 
-void calculateNormals(mesh_t& mesh)
+std::shared_ptr<Point> intersectMesh(const Point& start, const Vector& dir, const Vertex& v0, const Vertex& v1, const Vertex& v2)
 {
+	Vector E1;
+	E1.i = v1.x - v0.x;
+	E1.j = v1.y - v0.y;
+	E1.k = v1.z - v0.z;
 
+	Vector E2;
+	E2.i = v2.x - v0.x;
+	E2.j = v2.y - v0.y;
+	E2.k = v2.z - v0.z;
+
+	Vector T;
+	T.i = start.x - v0.x;
+	T.j = start.y - v0.y;
+	T.k = start.z - v0.z;
+
+	Vector P = cross(dir, E1);
+	Vector Q = cross(T, E1);
+
+	float coeff = 1.0f/dot(P, E1);
+	float t = coeff*dot(Q, E2);
+	float u = coeff*dot(P, T);
+	float v = coeff*dot(Q, dir);
+	if(u<0 || u>1 || v<0 || v>1 || u+v>1)
+		return nullptr;
+
+	std::shared_ptr<Point> result(new Point());
+	result->x = start.x + dir.i*t;
+	result->y = start.y + dir.j*t;
+	result->z = start.z + dir.k*t;
+
+	return result;
+}
+
+BoundingBox findBoundingBox(const vector<float>& vertices)
+{
+	BoundingBox result;
+	result.x_min = result.y_min = result.z_min = FLT_MAX;
+	result.x_max = result.y_max = result.z_max = -FLT_MAX;
+
+	for(int i=0; i < (int)vertices.size(); i += 3)
+	{
+		if(vertices[i] < result.x_min)
+			result.x_min = vertices[i];
+		if(vertices[i] > result.x_max)
+			result.x_max = vertices[i];
+
+		if(vertices[i+1] < result.y_min)
+			result.y_min = vertices[i+1];
+		if(vertices[i+1] > result.y_max)
+			result.y_max = vertices[i+1];
+
+		if(vertices[i+2] < result.z_min)
+			result.z_min = vertices[i+2];
+		if(vertices[i+2] > result.z_max)
+			result.z_max = vertices[i+2];
+	}
+
+	return result;
 }
